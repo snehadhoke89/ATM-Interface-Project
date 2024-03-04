5d4882ccfd19d6279702015f05f0a42307803de0
user <user@LAPTOP-2IKV4IP5>
2024-03-04 16:04:01 +0530

    project commit

---
+++ ATM Project/src/ATMSimulator/AtmOpInterface.java
@@ -0,0 +1,8 @@
+package ATMSimulator;
+
+public interface AtmOpInterface {
+    public void viewBalance();
+    public void withdrawAmount(double withdrawAmount);
+    public void depositAmount(double depositAmount );
+    public  void viewMiniStatement();
+}
